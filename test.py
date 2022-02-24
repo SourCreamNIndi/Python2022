8aa76689a6606e7b2d4416a0d1b289d6a51b9f74
SourCreamNIndi <niccam53@kingshigh.school.nz>
2022-02-25 12:21:45 +1300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ print("Hello world.")
 print("Nice")
 print("How are you")
 print(5*5)
-"""
+
+# 4.1 Print Excersises / P.g 13
+
 print("Nicholas")
 print("Mrs White")
 print("King's High")
@@ -14,3 +16,23 @@ print(658/7)
 print("A long time ago")
 print("Apex legends")
 print("Digi Tec")
+"""
+
+#Data Types/ 4.4 Excercises / p.g 17
+print("\n")
+print("Never gonna give you up\n Never gonna let you down\n Never gonna run around\n and desert you Never gonna make you cry")
+print("\n")
+print("Star Wars Episode 1 The Phantom menace\n Ex squeezee me.")
+print("\n")
+print("A string is a line of characters incased in quotation marks.")
+print("\n")
+print("An interger is a whole number.")
+print("\n")
+print("A float is the oposite of an interger because it includes fractions of numbers.")
+print("\n")
+print(543/3)
+
+
+
+
+
